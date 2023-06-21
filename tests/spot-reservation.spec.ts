e7b2977d6057a25265b2ab7f5e57cac96a9197d7
Anna Rompa <a.rompa@kainos.com>
2023-06-21 17:31:08 +0200

    change to correct class

--- tests/spot-reservation.spec.ts
+++ tests/spot-reservation.spec.ts
@@ -17,18 +17,16 @@ test('Reserve a spot', async ({ page }) => {
   await submitButton.click();
 
   await page.waitForResponse;
-  // await page.goto('https://studiojogapark.pl/strefaklienta/index.php?s=sala_3&date=&instructor=533758&type=HATHA_JOGA_0');
-  // const spotField = page.getByRole('cell', {name: /HATHA JOGA 0 20:00 - 21:15/}).first();
 
-  await page.goto('https://studiojogapark.pl/strefaklienta/index.php?s=sala_1');
-  const spotField = await page.getByRole('cell', {name: /HATHA JOGA I 10:00 - 11:15/}).first();
+//todo changing date - get current date and add 7 days
+  await page.goto('https://studiojogapark.pl/strefaklienta/index.php?s=sala_3&date=&instructor=533758&type=HATHA_JOGA_0&date=2023-06-28');
+  const spotField = page.getByRole('cell', {name: /HATHA JOGA 0 20:00 - 21:15/}).first();
   await spotField.click();
 
   const multisportRadioButton = await page.getByText('MULTISPORT');
-  // const acceptRegulations = await page.getByLabel(/regulamin/);
   const reserveButton = await page.getByText(/Zarezerwuj/);
 
-  
+
   await multisportRadioButton.check();
   await page.locator('#r2').check({ force: true });
   await reserveButton.click({ force: true });
